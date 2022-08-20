c90f300396f02f0bc300e16159e8e34d4dbaa33d
DAT <dat-1293@hotmail.com>
2022-08-21 00:32:21 +0800

    feat. react router & basic component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import FAQ from './components/FAQ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Welcome to React Router!</h1>
+      {/* 路由表 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="faq" element={<FAQ />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +27,58 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/faq">FAQ</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+// function FAQ() {
+//   return (
+//     <>
+//       <h2>FAQ</h2>
+//       <Link to="/">Home</Link>
+//     </>
+//   );
+// }
+
+function NotFound() {
+  return (
+    <>
+      <h2>不存在網址</h2>
+      <Link to="/">回到首頁</Link>
+    </>
   );
 }
 
